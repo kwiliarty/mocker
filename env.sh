e8071687c7a0c5f1c47637d390922605f68fad63
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2018-01-31 11:23:08 -0500

    adds a few aliases

--- env.sh
+++ env.sh
@@ -7,6 +7,10 @@ export MOODLE_DOCKER_WEB_PORT=8034
 export MOODLE_DOCKER_SELENIUM_VNC_PORT=5934
 
 # My own convenience stuff
-compose="${MOODLE_DOCKER_WWWROOT%/moodle}/bin/moodle-docker-compose"
-alias moodle-cron="${compose} exec webserver php admin/cli/cron.php"
-alias moodle-db="${compose} exec db mysql -u moodle -pm@0dl3ing moodle"
+
+alias mocker="${MOODLE_DOCKER_WWWROOT%/moodle}/bin/moodle-docker-compose"
+alias mocker-cron="mocker exec webserver php admin/cli/cron.php"
+alias mocker-db="mocker exec db mysql -u moodle -pm@0dl3ing moodle"
+alias mocker-down="mocker stop"
+alias mocker-up="mocker up -d"
+alias mocker-stop="mocker stop"
